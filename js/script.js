4c7be954ebc388713ac70ed0ed28038811a5c38d
pedroBenildo47 <landocapita47@gmail.com>
2024-04-06 21:05:54 +0300

    hh

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scroll > 80);
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
